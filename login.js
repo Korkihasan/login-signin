aca6d1061e9e3140a1ff190d35b3c1da0e473755
Korkihasan <hasankorkmazdc@outlook.com>
2022-11-02 14:01:51 +0300

    login-signin

--- login.js
+++ login.js
@@ -1,15 +1,24 @@
+const element = document.querySelector(".signdisplay");
+const element1 = document.querySelector(".logindisplay");
+var remove = document.getElementById("signdisplay");
+var remove1 = document.getElementById("logindisplay");
+
 function signup() {
   document.getElementById("signdisplay").style.display = "flex";
-  document.getElementById("logindisplay").style.display = "flex";
+  document.getElementById("logindisplay").style.display = "none";
 
-  const element = document.querySelector(".signdisplay");
-  element.classList.add("animate__animated", "animate__flipInY");
+  element.classList.add("animate__animated");
+  element.classList.add("animate__flipInY");
+  remove1.classList.remove("animate__animated");
+  remove1.classList.remove("animate__flipInY");
 }
 
 function login() {
-  document.getElementById("signdisplay").style.display = "flex";
+  document.getElementById("signdisplay").style.display = "none";
   document.getElementById("logindisplay").style.display = "flex";
 
-  const element1 = document.querySelector(".logindisplay");
-  element1.classList.add("animate__animated", "animate__flipInY");
+  element1.classList.add("animate__animated");
+  element.classList.add("animate__flipInY");
+  remove.classList.remove("animate__animated");
+  remove.classList.remove("animate__flipInY");
 }
