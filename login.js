62deb4afede5a18f656a0633a05fcfa14ceb04a9
Korkihasan <hasankorkmazdc@outlook.com>
2022-11-02 11:54:28 +0300

    login-signin

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function signup() {
   document.getElementById("logindisplay").style.display = "none";
 
   const element = document.querySelector(".signdisplay");
-  element.classList.add("animate__animated", "animate__flipInY");
+  element.classList.add("animate__animated", "animate__fadeInRight");
 }
 
 function login() {
@@ -11,5 +11,5 @@ function login() {
   document.getElementById("logindisplay").style.display = "flex";
 
   const element1 = document.querySelector(".logindisplay");
-  element1.classList.add("animate__animated", "animate__flipInY");
+  element1.classList.add("animate__animated", "animate__fadeInLeft");
 }
