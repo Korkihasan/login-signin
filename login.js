a1da45ce54000c24338a0b52c1560498eea65845
Korkihasan <hasankorkmazdc@outlook.com>
2022-11-02 11:40:52 +0300

    login-signin

---
+++ login.js
@@ -0,0 +1,15 @@
+function signup() {
+  document.getElementById("signdisplay").style.display = "block";
+  document.getElementById("logindisplay").style.display = "none";
+
+  const element = document.querySelector(".signdisplay");
+  element.classList.add("animate__animated", "animate__zoomInRight");
+}
+
+function login() {
+  document.getElementById("signdisplay").style.display = "none";
+  document.getElementById("logindisplay").style.display = "flex";
+
+  const element1 = document.querySelector(".logindisplay");
+  element1.classList.add("animate__animated", "animate__zoomInLeft");
+}
